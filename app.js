1b5671e152dec99920af202552c7162629549703
Naman Jain <jnaman475@gmail.com>
2022-09-12 19:23:18 +0530

    fix the code

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ const txtInput = document.querySelector("#txt-input");
 const btnTrnslt = document.querySelector("#btn-trnslt");
 const output = document.querySelector("#output");
 
-btnTrnslt.addEventListener("click", clickHandler) 
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
 
-function clickHandler (){
-    var inputText = txtInput.value;
+btnTrnslt.addEventListener("click", clickHandler);
 
-    fetch(getTranslationURL (inputText))
-    .then(response => response.json())
-    .then (json => {
-var translatedText = json.contents.translated;
-output.innerText = translatedText;
+function clickHandler() {
+  var inputText = txtInput.value;
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      output.innerText = translatedText;
     })
-    .catch(errorHandler)
-};
-
-function errorHandler(error){
-    console.log("error occured", error)
-    alert ("server is down my frend ! please come after sometime")
+    .catch(errorHandler);
 }
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text=" + text
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("server is down my frend ! please come after sometime");
 }
 
-var serverURL = "https://api.funtranslations.com/translate/yoda.json"
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
